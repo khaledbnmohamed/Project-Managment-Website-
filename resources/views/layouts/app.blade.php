979fb364516b629a908be9ebc03f6d93ac6a99bf
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-01-05 23:15:17 +0200

    Fixed the edit pages CSS problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,8 +36,9 @@
         {{-- Favicons css  for the nav bar icons--}}
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    
+
 
 </head>
 
